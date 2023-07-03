8c2eeb2ed4c3185c47c00e6ddcbd46a5a4f0b94b
shtwzrd <2846522+shtwzrd@users.noreply.github.com>
2023-07-03 14:40:28 +0200

    add layout for lily58

--- layout.h
+++ layout.h
@@ -3,7 +3,7 @@
 #define XXX KC_NO
 
 // Layout alias wrapper
-#define LAYOUT_36key(\
+#define LAYOUT_36key_ergo(\
      K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09,\
      K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19,\
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29,\
@@ -21,4 +21,20 @@ XXX, XXX, XXX, XXX, K32,                          K37, XXX, XXX, XXX, XXX,\
 )
 
 // Layout alias wrapper for use in json keymap
-#define LAYOUT_36key_w(...) LAYOUT_36key(__VA_ARGS__)
+#define LAYOUT_36key_ergo_w(...) LAYOUT_36key_ergo(__VA_ARGS__)
+
+#define LAYOUT_36key_lily(\
+     K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09,\
+     K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19,\
+     K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29,\
+               K32, K33, K34,                K35, K36, K37 \
+)\
+LAYOUT(\
+XXX, XXX, XXX, XXX, XXX, XXX,                XXX, XXX, XXX, XXX, XXX, XXX,\
+XXX, K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, XXX,\
+XXX, K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, XXX,\
+XXX, K20, K21, K22, K23, K24, XXX,      XXX, K25, K26, K27, K28, K29, XXX,\
+               XXX, K32, K33, K34,      K35, K36, K37, XXX\
+)
+
+#define LAYOUT_36key_lily_w(...) LAYOUT_36key_lily(__VA_ARGS__)
